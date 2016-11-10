a5a9f7401ff4fcf30fe9fc426e6842c325f1480c
zoubass <zoubas@seznam.cz>
2016-11-10 14:17:24 +0100

    Implemented RowCallBackHandler for optimal memory usage.

--- src/test/java/cz/zoubelu/integration/DatabaseConnectionTest.java
+++ src/test/java/cz/zoubelu/integration/DatabaseConnectionTest.java
@@ -1,15 +1,11 @@
 package cz.zoubelu.integration;
 
-
-import cz.zoubelu.domain.Message;
-import cz.zoubelu.utils.TimeRange;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 /**
  * Created by zoubas
@@ -26,16 +22,16 @@ public class DatabaseConnectionTest extends AbstractTest {
         Timestamp start = Timestamp.valueOf("2016-06-01 00:00:00.0");
         Timestamp end = Timestamp.valueOf("2016-06-07 23:59:00.0");
 
-        List<Message> messages = informaRepository.getInteractionData(new TimeRange(start,end));
-        Assert.assertEquals(8611, messages.size());
+        Long dataSize = jdbcTemplate.queryForObject("select count(*) from Message where request_time between ? and ?",Long.class,start,end);
+        Assert.assertEquals(8611, dataSize.longValue());
     }
 
     @Test
     public void testDataShouldBeInSpecifiedTimeRange() {
-        List<Timestamp> minDate = jdbcTemplate.queryForList("select MIN(request_time) from MESSAGE", Timestamp.class);
-        List<Timestamp> maxDate = jdbcTemplate.queryForList("select MAX(request_time) from MESSAGE", Timestamp.class);
-        Assert.assertEquals("[2016-06-01 00:09:52.245]",minDate.toString());
-        Assert.assertEquals("[2016-06-26 01:00:18.991]",maxDate.toString());
+        Timestamp minDate = jdbcTemplate.queryForObject("select MIN(request_time) from MESSAGE", Timestamp.class);
+        Timestamp maxDate = jdbcTemplate.queryForObject("select MAX(request_time) from MESSAGE", Timestamp.class);
+        Assert.assertEquals("2016-06-01 00:09:52.245",minDate.toString());
+        Assert.assertEquals("2016-06-26 01:00:18.991",maxDate.toString());
     }
 
 }
