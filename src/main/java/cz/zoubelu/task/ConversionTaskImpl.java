9e3fcceabaec4b34878b8e9d2ac608c9178beee8
zoubass <zoubas@seznam.cz>
2016-11-10 14:17:37 +0100

    Implemented RowCallBackHandler for optimal memory usage.

--- src/main/java/cz/zoubelu/task/ConversionTaskImpl.java
+++ src/main/java/cz/zoubelu/task/ConversionTaskImpl.java
@@ -1,10 +1,16 @@
 package cz.zoubelu.task;
 
 import cz.zoubelu.service.DataConversion;
+import cz.zoubelu.utils.ConversionError;
+import cz.zoubelu.utils.CsvFileUtils;
 import cz.zoubelu.utils.DateUtils;
 import it.sauronsoftware.cron4j.Task;
 import it.sauronsoftware.cron4j.TaskExecutionContext;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 /**
  * Created by zoubas on 14.7.2016.
@@ -12,17 +18,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ConversionTaskImpl extends Task implements ConversionTask {
 
 	@Autowired
-	private DataConversion conversionService;
+	private DataConversion dataConversion;
 
+	private final Logger log = Logger.getLogger(getClass());
+
+	@Value("${table.prefix}")
 	private String tablePrefix;
-
-	public ConversionTaskImpl(String tablePrefix){
-		this.tablePrefix=tablePrefix;
-	}
 
 	@Override
 	public void execute(TaskExecutionContext taskExecutionContext) throws RuntimeException {
-		String tableName = tablePrefix + DateUtils.getTableSuffix();
-		conversionService.convertData(tableName);
+		log.info("Starting conversion.");
+		long startTime = System.currentTimeMillis();
+
+		String tableName = tablePrefix + DateUtils.getYearMonthSuffix();
+		List<ConversionError> errors = dataConversion.convertData(tableName);
+		CsvFileUtils.savelist();
+
+		long stopTime = System.currentTimeMillis();
+		log.info(String.format("Conversion took %s ms.", stopTime - startTime));
+		log.info("Conversion ended with " + errors.size() + " errors.");
 	}
 }
