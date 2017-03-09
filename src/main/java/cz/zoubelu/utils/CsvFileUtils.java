0f050008a3dabd7c32e601dc5675aaae2d8fd87a
zoubas <zoubas@seznam.cz>
2017-03-09 20:24:59 +0100

    loading and saving system lIST

--- src/main/java/cz/zoubelu/utils/CsvFileUtils.java
+++ src/main/java/cz/zoubelu/utils/CsvFileUtils.java
@@ -3,8 +3,11 @@ package cz.zoubelu.utils;
 import cz.zoubelu.codelist.SystemApp;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,24 +17,48 @@ public class CsvFileUtils {
     private static final Logger log = Logger.getLogger("CsvUtils");
     private static final String DELIMITER = ";";
 
-    public static void saveList(List<SystemApp> systems) {
-        String file = "systemList" + DateUtils.getYearMonthSuffix() + ".csv";
-        log.info("Saving system list: " + file);
+    private String fileLocation;
 
-        if (!systems.isEmpty() && systems != null) {
-            try {
-                FileWriter writer = new FileWriter(file);
-                for (SystemApp s : systems) {
-                    writer.write(s.getName() + DELIMITER + s.getId() + DELIMITER + "\n");
-                }
-                writer.flush();
-                writer.close();
-                log.info("Systems list successfully saved.");
-            } catch (IOException e) {
-                log.error("Failed to cache system list into directory: " + file, e);
+
+    public static void save(List<SystemApp> systems, String fileLocation) {
+
+        log.info("Saving system list: " + fileLocation);
+
+        try {
+            FileWriter writer = new FileWriter(fileLocation);
+            for (SystemApp s : systems) {
+                writer.write(s.getName() + DELIMITER + s.getId() + "\n");
             }
-        } else {
-            log.error("Failed to save system list because it's empty.");
+            writer.flush();
+            writer.close();
+            log.info("Systems list successfully saved.");
+        } catch (IOException e) {
+            log.error("Failed to cache system list into directory: " + fileLocation, e);
         }
     }
+
+    public static List<SystemApp> load(String fileLocation) {
+        List<SystemApp> systems = new ArrayList<SystemApp>();
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(fileLocation));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] params = line.split(DELIMITER);
+                String name = params[0];
+                Integer id = Integer.valueOf(params[1]);
+                systems.add(new SystemApp(name, id));
+            }
+
+        } catch (IOException e) {
+            log.error("Failed to load file: " + fileLocation, e);
+        } catch (NumberFormatException e) {
+            log.error("Cannot load list, the system ID is not a valid number. Check the csv list!");
+        }
+        return systems;
+    }
+
+
 }
